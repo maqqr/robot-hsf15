e35f47b6c25e799be4b3e0290155bfc775bf97c8
maqqr <maqqr@users.noreply.github.com>
2015-02-07 11:39:25 +0200

    Robot can drive atleast one lap in simulation now

--- robotlogic/states.c
+++ robotlogic/states.c
@@ -1,5 +1,20 @@
 #include "states.h"
 
+#define FAST 1.0f
+#define SLOW 0.65f
+
+#define SLOW_LIMIT 45.0f
+#define WALL_LIMIT 25.0f
+
+int check_error(robot_input_t input)
+{
+  if (input.front_sensor < 5.0f)
+  {
+    state = state_error;
+    return 1;
+  }
+  return 0;
+}
 
 /* Initial robot state. */
 robot_output_t state_init_func(robot_input_t input)
@@ -11,15 +26,26 @@ robot_output_t state_init_func(robot_input_t input)
 
 robot_output_t state_forward_func(robot_input_t input)
 {
-  /* All code of this function is temporary testing code. */
-  robot_output_t output;
-  output.left = 1.0f;
-  output.right = 1.0f;
-  
-  if (input.front_sensor < 40.0f)
+  if (check_error(input))
   {
-    output.left = 0.0f;
-    output.right = 0.0f;
+    return zerooutput;
+  }
+
+  robot_output_t output;
+  output.left = FAST;
+  output.right = FAST;
+
+  // Slow down near a wall.
+  if (input.front_sensor < SLOW_LIMIT)
+  {
+    output.left = SLOW;
+    output.right = SLOW;
+  }
+  
+  // Turn when too close to a wall.
+  if (input.front_sensor < WALL_LIMIT)
+  {
+    state = input.left_sensor > input.right_sensor? state_left : state_right;
   }
 
   return output;
@@ -27,18 +53,45 @@ robot_output_t state_forward_func(robot_input_t input)
 
 robot_output_t state_left_func(robot_input_t input)
 {
-  /* TODO */
-  return zerooutput;
+  robot_output_t output;
+  output.left = -1.0f;
+  output.right = 1.0f;
+
+  // Continue forwards when there is enough space.
+  if (input.front_sensor < WALL_LIMIT + 30.0f)
+  {
+    state = state_forward;
+  }
+
+  return output;
 }
 
 robot_output_t state_right_func(robot_input_t input)
 {
-  /* TODO */
-  return zerooutput;
+  robot_output_t output;
+  output.left = 1.0f;
+  output.right = -1.0f;
+
+  // Continue forwards when there is enough space.
+  if (input.front_sensor < WALL_LIMIT + 10.0f)
+  {
+    state = state_forward;
+  }
+
+  return output;
 }
 
 robot_output_t state_error_func(robot_input_t input)
 {
-  /* TODO */
-  return zerooutput;
+  // Something is wrong, go backwards.
+  robot_output_t output;
+  output.left = -1.0f;
+  output.right = -1.0f;
+
+  if (input.front_sensor > 30.0f)
+  {
+    state = state_forward;
+  }
+
+  return output;
 }
